f98a8fb6ba037a5bdcc38391885e5f2b62d51fd0
sanji118 <sanjishifa32@gmail.com>
2025-05-22 13:54:30 +0600

    added json file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors');
-const dotenv = require('dotenv')
+require('dotenv').config();
 const {MongoClient, ServerApiVersion} = require('mongodb')
 
 const app = express();
@@ -13,11 +13,48 @@ app.use(cors());
 app.use(express.json());
 
 
+const reviews = require('./reviews.json')
+
 app.get('/', (req, res)=>{
     res.send('GameNest server is running !');
 })
+
+app.get('/reviews', async(req, res)=>{
+        res.json(reviews);
+})
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.95qfhdq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    
+    const reviewCollection = client.db('reviewsDB').collection('reviews')
+
+    
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 app.listen(port, ()=>{
     console.log(`GameNest Server is running on port : ${port}`)
 })
+
